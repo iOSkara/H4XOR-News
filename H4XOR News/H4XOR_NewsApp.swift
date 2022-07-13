51d42bbeffd89643429403987d66f737aa71d72d
Roman Vasyltsov <rgamer402@gmail.com>
2022-07-13 17:36:10 +0300

    Initial Commit

---
+++ H4XOR News/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  H4XOR News
+//
+//  Created by Mac on 13.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
